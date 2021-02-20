1d6d57144aabcc5cb1fdeb3497e424efed8315f7
OluchiC <Oluchi.j.c@Gmail.com>
2021-02-20 17:38:03 -0500

    Added Lab1.pyt
    Just to have a des.

---
+++ Lab1.py
@@ -0,0 +1,11 @@
+sentence = 'I can’t wait to get to School_Name! Love the idea of meeting new Noun and making new Noun! I know that when Number years pass, I will be Age and I will have a degree in Profession. I hope to make my family proud! Am I done with this MadLib Yet?: Boolean.'
+
+school_name = input('What\'s the school name?')
+meeting = input('What\'s the thing you\'re meeting at school?')
+making = input('What\'s the thing you\'re making at school?')
+number = str(input('What\'s the number?'))
+age = str(input('What\'s the Age?'))
+dream = input('What\'s you\'r dream profession?')
+decision = str(input('What\'s Are you done (True/False)?'))
+
+print('I can’t wait to get to ',school_name,'! Love the idea of meeting new ',meeting,' and making new ',making,'! I know that when ',number,' years pass, I will be ',age,' and I will have a degree in ',dream,'. I hope to make my family proud! Am I done with this MadLib Yet?:',decision)
